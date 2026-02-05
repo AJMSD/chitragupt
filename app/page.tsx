7b9eed95f93c313d8dca6b25f4c051628642d039
Aman Jain <aman.jain030704@gmail.com>
2026-02-04 20:46:10 -0600

    changed heighet of charts

--- app/page.tsx
+++ app/page.tsx
@@ -761,7 +761,7 @@ export default function Home() {
             <div className="mt-2">
               <LineChart
                 data={storageHistory.length ? storageHistory : [0, 0]}
-                height={300}
+                height={280}
                 stroke="#f97316"
                 fill="rgba(249,115,22,0.22)"
               />
